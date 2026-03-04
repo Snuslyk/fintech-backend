f9579c4212b92654f11effcc8a2246dab8ec9714
Snuslyk <127945008+Snuslyk@users.noreply.github.com>
2026-03-05 01:46:08 +0300

    feat: multiple errors for one field

--- configs/validation-exception.config.ts
+++ configs/validation-exception.config.ts
@@ -2,10 +2,16 @@ import { ValidationError } from 'class-validator'
 import { HttpException, HttpStatus } from '@nestjs/common'
 
 export const validationExceptionConfig = (errors: ValidationError[]) => {
-  const message = errors.reduce<Record<string, string>>((acc, error) => {
+  const message = errors.reduce<Record<string, string | string[]>>((acc, error) => {
     const { property, constraints } = error
-    if (constraints)
-      acc[property] = Object.values(constraints)[0]
+    if (constraints) {
+      const constraintValues = Object.values(constraints)
+      if (constraintValues.length === 1) {
+        acc[property] = constraintValues[0]
+      } else {
+        acc[property] = constraintValues
+      }
+    }
 
     return acc
   }, {})
--- src/modules/user/user.dto.ts
+++ src/modules/user/user.dto.ts
@@ -1,9 +1,10 @@
-import { IsNotEmpty, IsString } from 'class-validator'
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
 
 export class UserDto {}
 
 export class RegisterDto {
   @IsString({ message: 'Имя должно быть строкой!' })
+  @IsEmail(undefined, { message: 'Имя должно быть в формате Email!' })
   @IsNotEmpty()
   name: string
 
