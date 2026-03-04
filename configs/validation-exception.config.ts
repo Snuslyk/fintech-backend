4d083023dc311ee62225610038b3732244be4025
Snuslyk <127945008+Snuslyk@users.noreply.github.com>
2026-03-05 02:29:27 +0300

    chore: value array better recognize

--- configs/validation-exception.config.ts
+++ configs/validation-exception.config.ts
@@ -5,12 +5,8 @@ export function ValidationExceptionConfig(errors: ValidationError[]) {
   const message = errors.reduce<Record<string, string | string[]>>((acc, error) => {
     const { property, constraints } = error
     if (constraints) {
-      const constraintValues = Object.values(constraints)
-      if (constraintValues.length === 1) {
-        acc[property] = constraintValues[0]
-      } else {
-        acc[property] = constraintValues
-      }
+      const values = Object.values(constraints)
+      acc[property] = values.length === 1 ? values[0] : values
     }
 
     return acc
